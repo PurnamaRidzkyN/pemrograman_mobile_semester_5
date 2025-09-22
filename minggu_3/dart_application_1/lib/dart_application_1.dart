d08885bc664530f285269d03b306a44e73c49bde
Purnama Ridzky Nugraha <purnamanugraha492@gmail.com>
2025-09-22 16:59:02 +0700

    feat: Add prime checking function to the Dart application

--- minggu_3/dart_application_1/lib/dart_application_1.dart
+++ minggu_3/dart_application_1/lib/dart_application_1.dart
@@ -10,3 +10,14 @@ int faktorial(int n) {
 
   return hasil;
 }
+bool cekPrima(int n) {
+  if (n < 2) return false;
+
+  for (int i = 2; i <= n ~/ 2; i++) {
+    if (n % i == 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
